87e758adfe09b10ce382df64f06962ca120f058a
GouravMankar <gouravmankar@hotmail.com>
2025-12-18 00:03:09 +0530

    [Gourav Mankar] : added a program for finding the Total price from the given quantity and per unit price in my central repository

---
+++ gcr-codebase/programming-elements/level-2/TotalPurchasePrice.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class TotalPurchasePrice {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking Input
+        System.out.print("Enter unit price (INR): ");
+        double unitPrice = sc.nextDouble();
+
+        System.out.print("Enter quantity: ");
+        int quantity = sc.nextInt();
+
+        // Total price Calculation
+        double totalPrice = unitPrice * quantity;
+
+        // Printing Output
+        System.out.println("The total purchase price is INR " + totalPrice +" if the quantity " + quantity +" and unit price is INR " + unitPrice );
+
+        sc.close();
+    }
+}
