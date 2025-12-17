d885af1d247cc2b912f419dc685a31d1061d1fa3
GouravMankar <gouravmankar@hotmail.com>
2025-12-17 19:12:54 +0530

    [Gourav Mankar] : added a program for calculating sams average marks in my central repository

---
+++ gcr-codebase/programming-elements/level-1/SamsAverageMarks.java
@@ -0,0 +1,16 @@
+public class SamsAverageMarks {
+    public static void main(String[] args) {
+        //Assigning the value to variables
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        
+        // Calculating the total marks and average
+        int total = maths + physics + chemistry;
+        double average = total / 3.0;
+
+        
+        // Printing the values
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
