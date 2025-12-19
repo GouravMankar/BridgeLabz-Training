523c19d737a9d9e3e14d37f8ab3e1ac4eac5c905
GouravMankar <gouravmankar@hotmail.com>
2025-12-20 00:11:27 +0530

    [Gourav Mankar] : added a program for calculating the BMI using the array in my central repository

---
+++ gcr-codebase/arrays/level-2/BMICalculationUsingArray.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class BMICalculationUsingArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // taking inputs from the user
+        System.out.print("Enter number of persons: ");
+        int n = scanner.nextInt();
+        double[] weight = new double[n];
+        double[] height = new double[n];
+        double[] bmi = new double[n];
+        String[] status = new String[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter weight of person " + (i + 1) + ": ");
+            weight[i] = scanner.nextDouble();
+            System.out.print("Enter height of person " + (i + 1) + ": ");
+            height[i] = scanner.nextDouble()/100;
+        }
+
+        // calculating BMI and status
+        for (int i = 0; i < n; i++) {
+            bmi[i] = weight[i] / (height[i] * height[i]);
+            if (bmi[i] < 18.5) status[i] = "Underweight";
+            else if (bmi[i] <=24.9) status[i] = "Normal";
+            else if (bmi[i] <=39.9) status[i] = "Overweight";
+            else status[i] = "Obese";
+        }
+
+        // displaying the result
+        for (int i = 0; i < n; i++) {
+            System.out.println("Person " + (i + 1) + ": Height=" + height[i] +
+                               ", Weight=" + weight[i] +
+                               ", BMI=" + bmi[i] +
+                               ", Status=" + status[i]);
+        }
+
+        scanner.close();
+    }
+}
