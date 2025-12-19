bc045a9fa3241bf1a3df08ea60817070f630b02e
GouravMankar <gouravmankar@hotmail.com>
2025-12-19 16:29:49 +0530

    [Gourav Mankar] : added a program for finding odd and even number of an array in my central repository

---
+++ gcr-codebase/arrays/level-1/OddEvenUsingArrays.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class OddEvenUsingArrays {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // taking inputs from the user
+        System.out.print("Enter a natural number: ");
+        int number = scanner.nextInt();
+
+        // calculating odd and even arrays
+        if (number <= 0) {
+            System.out.println("Error! Not a natural number.");
+            scanner.close();
+            return;
+        }
+
+        int[] odd = new int[number / 2 + 1];
+        int[] even = new int[number / 2 + 1];
+        int oddIndex = 0, evenIndex = 0;
+
+        for (int i = 1; i <= number; i++) {
+            if (i % 2 == 0) {
+                even[evenIndex++] = i;
+            } else {
+                odd[oddIndex++] = i;
+            }
+        }
+
+        // displaying the result
+        System.out.print("Odd numbers: ");
+        for (int i = 0; i < oddIndex; i++) System.out.print(odd[i] + " ");
+        System.out.print("\nEven numbers: ");
+        for (int i = 0; i < evenIndex; i++) System.out.print(even[i] + " ");
+
+        scanner.close();
+    }
+}
