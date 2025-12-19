939e9bf3c6c7fd5c5067bec8ced8d91609ee724d
GouravMankar <gouravmankar@hotmail.com>
2025-12-20 00:16:26 +0530

    [Gourav Mankar] : added a program for finding largest and second largest digit in a given number using a dynamic array in my central repository

---
+++ gcr-codebase/arrays/level-2/LargestAndSecondLargetDigitsDynamic.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class LargestAndSecondLargetDigitsDynamic {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // taking inputs from the user
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+
+        // calculating digits with dynamic array resizing
+        int maxDigit = 10;
+        int[] digits = new int[maxDigit];
+        int index = 0;
+
+        while (number != 0) {
+            if (index == maxDigit) {
+                maxDigit += 10;
+                int[] temp = new int[maxDigit];
+                System.arraycopy(digits, 0, temp, 0, digits.length);
+                digits = temp;
+            }
+            digits[index++] = number % 10;
+            number /= 10;
+        }
+
+        int largest = 0, secondLargest = 0;
+        for (int i = 0; i < index; i++) {
+            if (digits[i] > largest) {
+                secondLargest = largest;
+                largest = digits[i];
+            } else if (digits[i] > secondLargest && digits[i] != largest) {
+                secondLargest = digits[i];
+            }
+        }
+
+        // displaying the result
+        System.out.println("Largest digit = " + largest);
+        System.out.println("Second Largest digit = " + secondLargest);
+
+        scanner.close();
+    }
+}
