cc1461c167aac4231b8ec66527cec144f4bb1e43
GouravMankar <gouravmankar@hotmail.com>
2025-12-26 02:05:43 +0530

    [Gourav Mankar] added a program to check the prime number

--- gcr-codebase/extras/problem-statements/PrimeChecker.java
+++ gcr-codebase/extras/problem-statements/PrimeChecker.java
@@ -9,7 +9,7 @@ public class PrimeChecker {
             if (n % i == 0) return false;
         }
         return true;
-    }
+    } 
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
