2bf061c41c06b0144d32dbdb3d8316cd6e319756
GouravMankar <gouravmankar@hotmail.com>
2025-12-26 02:12:45 +0530

    [Gourav Mankar] added a program to find most frequent character

--- gcr-codebase/extras/string-handling/MostFrequentCharacter.java
+++ gcr-codebase/extras/string-handling/MostFrequentCharacter.java
@@ -5,7 +5,7 @@ public class MostFrequentCharacter {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
+ 
         // take input string
         System.out.println("Enter a string:");
         String str = sc.nextLine();
