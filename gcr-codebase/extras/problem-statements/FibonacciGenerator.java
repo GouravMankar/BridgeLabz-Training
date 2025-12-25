8414702504700e75889597d0f60c9630f816e159
GouravMankar <gouravmankar@hotmail.com>
2025-12-26 02:01:08 +0530

    [Gourav Mankar] added a program to generate fibonacci sequence

--- gcr-codebase/extras/problem-statements/FibonacciGenerator.java
+++ gcr-codebase/extras/problem-statements/FibonacciGenerator.java
@@ -7,7 +7,7 @@ public class FibonacciGenerator {
         int a = 0, b = 1;
         System.out.print("Fibonacci Sequence: ");
         for (int i = 0; i < terms; i++) {
-            System.out.print(a + " ");
+            System.out.print(a + " ");   
             int next = a + b;
             a = b;
             b = next;
