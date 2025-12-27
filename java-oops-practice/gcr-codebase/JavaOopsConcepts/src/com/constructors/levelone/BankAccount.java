980651ff8230c81d75de762ee588e4f952520034
GouravMankar <gouravmankar@hotmail.com>
2025-12-27 23:56:56 +0530

    [Gourav Mankar] : added a program of bank account management

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructors/levelone/BankAccount.java
@@ -0,0 +1,23 @@
+package com.constructors.levelone;
+
+//BankAccount class demonstrates access modifiers
+class BankAccount {
+ public int accountNumber;
+ protected String accountHolder;
+ private double balance;
+
+ BankAccount(int accountNumber, String accountHolder, double balance) {
+     this.accountNumber = accountNumber;
+     this.accountHolder = accountHolder;
+     this.balance = balance;
+ }
+
+ // Getter and Setter for private balance
+ public double getBalance() {
+     return balance;
+ }
+
+ public void setBalance(double balance) {
+     this.balance = balance;
+ }
+}
