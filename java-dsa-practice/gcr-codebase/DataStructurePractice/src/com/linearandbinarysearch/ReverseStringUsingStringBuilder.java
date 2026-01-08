a80ce2e16d150a092c2da2b32c1570f274787f80
GouravMankar <gouravmankar@hotmail.com>
2026-01-08 11:55:00 +0530

    [Gourav Mankar] : added a program to reverse string using string builder.

---
+++ java-dsa-practice/gcr-codebase/DataStructurePractice/src/com/linearandbinarysearch/ReverseStringUsingStringBuilder.java
@@ -0,0 +1,26 @@
+package com.linearandbinarysearch;
+
+public class ReverseStringUsingStringBuilder {
+
+    public static void main(String[] args) {
+
+        String input = "hello";
+
+        
+        StringBuilder sb = new StringBuilder();
+
+        
+        sb.append(input);
+
+       
+        sb.reverse();
+
+        
+        String output = sb.toString();
+
+        
+        System.out.println("Original String: " + input);
+        System.out.println("Reversed String: " + output);
+    }
+}
+
