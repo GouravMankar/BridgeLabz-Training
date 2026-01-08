c3156b5698eed306e8cb011058a91880e5d68649
GouravMankar <gouravmankar@hotmail.com>
2026-01-08 11:11:53 +0530

    [Gourav Mankar] : added a DSA program to find counting sort students ages.

---
+++ java-dsa-practice/gcr-codebase/DataStructurePractice/src/com/sortings/CountingSortStudentAges.java
@@ -0,0 +1,37 @@
+package com.sortings;
+
+public class CountingSortStudentAges {
+
+    public static void main(String[] args) {
+
+        int[] ages = {12, 15, 10, 14, 18, 11, 13, 15, 10};
+
+        int minAge = 10;
+        int maxAge = 18;
+
+        int range = maxAge - minAge + 1;
+
+        int[] count = new int[range];
+
+       
+        for (int i = 0; i < ages.length; i++) {
+            count[ages[i] - minAge]++;
+        }
+
+       
+        int index = 0;
+        for (int i = 0; i < range; i++) {
+            while (count[i] > 0) {
+                ages[index] = i + minAge;
+                index++;
+                count[i]--;
+            }
+        }
+
+        
+        System.out.println("Student ages in ascending order:");
+        for (int i = 0; i < ages.length; i++) {
+            System.out.print(ages[i] + " ");
+        }
+    }
+}
