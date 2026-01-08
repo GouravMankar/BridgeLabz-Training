7a0f8f7932b77c4dc84153a152e51cd708be3ba4
GouravMankar <gouravmankar@hotmail.com>
2026-01-08 11:53:37 +0530

    [Gourav Mankar] : added a program to remove duplicates using string builder.

---
+++ java-dsa-practice/gcr-codebase/DataStructurePractice/src/com/linearandbinarysearch/RemoveDuplicatesUsingStringBuilder.java
@@ -0,0 +1,35 @@
+package com.linearandbinarysearch;
+
+import java.util.HashSet;
+
+
+public class RemoveDuplicatesUsingStringBuilder {
+
+    public static void main(String[] args) {
+
+        String input = "programming";
+
+        StringBuilder sb = new StringBuilder();
+        HashSet<Character> set = new HashSet<>();
+
+        
+        for (int i = 0; i < input.length(); i++) {
+
+            char ch = input.charAt(i);
+
+            
+            if (!set.contains(ch)) {
+                sb.append(ch);
+                set.add(ch);
+            }
+        }
+
+        String result = sb.toString();
+
+        // display output
+        System.out.println("Original String: " + input);
+        System.out.println("String without duplicates: " + result);
+    }
+}
+
+
