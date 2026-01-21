c7a568633d03a5abfc10a09c2cf3cbb0ca70af9f
GouravMankar <gouravmankar@hotmail.com>
2026-01-21 13:14:41 +0530

    [Gourav Mankar] : added a program of collections to find the nth element from the end in a list

---
+++ java-collections-practice/gcr-codebase/javacollectionspractice/src/com/collections/list/FindNthFromEnd.java
@@ -0,0 +1,40 @@
+package com.collections.list;
+
+public class FindNthFromEnd {
+	private static Node getNthFromEnd(Node head, int n) {
+		Node temp = head;
+		
+		for(int i = 0; i < n; i++) {
+			if(temp == null) {
+				System.out.println("Please enter a valid number");
+				System.exit(1);
+			}
+			temp = temp.next;
+		}
+		
+		while(temp != null) {
+			temp = temp.next;
+			head = head.next;
+		}
+		
+		return head;
+	}
+	
+	public static void main(String[] args) {
+		Node head = new Node(0);
+		Node node2 = new Node(1);
+		Node node3 = new Node(2);
+		Node node4 = new Node(3);
+		Node node5= new Node(4);
+		Node node6=new Node(5);
+		
+		head.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		node4.next = node5;
+		node5.next=node6;
+		
+		Node nthFromEndNode = getNthFromEnd(head, 1);
+		System.out.println(nthFromEndNode.data);
+	}
+}
