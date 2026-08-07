81a1a8f3d360ada8c357c68966e38a0220e796fb
GouravMankar <gouravmankar@hotmail.com>
2026-08-07 09:43:58 +0530

    [Gourav Mankar] : added a HackerRank problem solution called Quick sort two partition.

---
+++ Refresher-Training/hackerrank-codebase/HackerRankCodeBase/src/com/day05/QuickSortTwoPartition.java
@@ -0,0 +1,56 @@
+package com.day05;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class QuickSortTwoPartition {
+	public static List<Integer> quickSort(List<Integer> arr) {
+		if (arr.size() <= 1) {
+			return arr;
+		}
+		int pivot = arr.get(0);
+		List<Integer> left = new ArrayList<>();
+		List<Integer> equal = new ArrayList<>();
+		List<Integer> right = new ArrayList<>();
+		for (int num : arr) {
+			if (num < pivot) {
+				left.add(num);
+
+			} else if (num > pivot) {
+				right.add(num);
+			} else {
+				equal.add(num);
+			}
+		}
+		List<Integer> leftSorted = quickSort(left);
+		List<Integer> rightSorted = quickSort(right);
+		List<Integer> result = new ArrayList<>();
+		result.addAll(leftSorted);
+		result.addAll(equal);
+		result.addAll(rightSorted);
+		if (result.size() > 1) {
+			printList(result);
+		}
+		return result;
+	}
+
+	public static void printList(List<Integer> arr) {
+		for (int val : arr) {
+			System.out.print(val + " ");
+		}
+		System.out.println();
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		List<Integer> arr = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			arr.add(sc.nextInt());
+		}
+		quickSort(arr);
+		sc.close();
+	}
+}
