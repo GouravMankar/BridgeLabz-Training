a2aefeeb556243b8475014a5a33d029ed3f2c73f
GouravMankar <gouravmankar@hotmail.com>
2026-08-11 09:04:45 +0530

    [Gourav Mankar] : added a HackerRank problem solution called Fibonacci number using recursion.

---
+++ Refresher-Training/hackerrank-codebase/HackerRankCodeBase/src/com/day07/FibonacciNumber.java
@@ -0,0 +1,19 @@
+package com.day07;
+
+public class FibonacciNumber {
+	public static int fibonacci(int n) {
+		if (n == 0 || n == 1) {
+			return n;
+		}
+		return fibonacci(n - 1) + fibonacci(n - 2);
+	}
+
+	public static void main(String[] args) {
+
+		int n = 8;
+		for (int i = 0; i <= n; i++) {
+			System.out.println(fibonacci(i));
+		}
+
+	}
+}
