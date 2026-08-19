9f710cfe58eef00e414236ad20536803c10645c0
GouravMankar <gouravmankar@hotmail.com>
2026-08-19 16:34:53 +0530

    [Gourav Mankar] : added a HackerRank problem solution called java inharitance 2.

---
+++ Refresher-Training/hackerrank-codebase/HackerRankCodeBase/src/com/day14/JavaInharitance2.java
@@ -0,0 +1,22 @@
+package com.day14;
+
+//Write your code here
+class Arithmetic {
+	public int add(int a, int b) {
+		return a + b;
+	}
+}
+
+class Adder extends Arithmetic {
+}
+
+public class JavaInharitance2 {
+	public static void main(String[] args) {
+
+		Adder a = new Adder();
+
+		System.out.println("My superclass is: " + a.getClass().getSuperclass().getName());
+
+		System.out.print(a.add(10, 32) + " " + a.add(10, 3) + " " + a.add(10, 10) + "\n");
+	}
+}
